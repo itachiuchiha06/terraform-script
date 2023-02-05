0d0759417b041bfb373e9b3c7d7167209fcaca38
itachi uchiha <114676214+itachiuchiha06@users.noreply.github.com>
2023-02-05 16:51:57 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,6 @@
 # update provider
 # AWS as my provider
 
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAXVIZWZTOAT3X64G5"
-  secret_key = "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"
-}
-
 # VPC
 
 resource "aws_vpc" "ram" {
